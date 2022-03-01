a1b911343768ac2c319f6ea122e179cb5033a76c
nasirishokoh <shukoh.nasiri@gmail.com>
2022-03-01 15:22:39 -0800

    first commit

---
+++ Trees.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int ids[100],size,c,high,low,mid,item,flag=0;
+	cout<<"How many elements you want to insert: "<<endl;
+	cin>>size;
+	for(c=0;c<size;c++){
+		cout<<"Enter element for array location:  "<<c<<"   ";
+		cin>>ids[c];
+	}
+	cout<<"Enter element for searching: ";
+	cin>>item;
+	low=0;
+	high=size-1;
+	while(low<=high){
+		mid=(low+high)/2;
+		if(item==ids[mid])
+		{
+			cout<<"Item found at location: "<<mid;
+			break;
+			//flag=flag+1;
+		}
+		else if(item>ids[mid]){
+			low=mid+1;
+		}
+		else{
+			high=mid-1;
+		}
+	}
+	if(low>high){
+		cout<<"item not found here";
+	}	
+}
